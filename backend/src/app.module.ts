76009f60eec7f46ddf47761482581dd08ad1aa6a
shiliaicha0-oss <shiliaicha0@gmail.com>
2026-04-12 17:05:30 +0200

    configuration du backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,51 @@
+import {
+  Module,
+  RequestMethod,
+  type MiddlewareConsumer,
+  type NestModule,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { BooksModule } from './books/books.module';
+import { FirstMiddleware } from './middlewares/first/first.middleware';
+import { SecondMiddleware } from './middlewares/second/second.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TasksModule,
+    BooksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: '',
+      database: 'isids26',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    ConfigModule.forRoot({
+  isGlobal: true,
+})
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(FirstMiddleware).forRoutes(''); // appliqué pour toutes les routes
+    // consumer.apply(FirstMiddleware).forRoutes('tasks/*'); // appliqué pour toutes les routes
+    consumer.apply(SecondMiddleware).forRoutes('');
+    consumer.apply(FirstMiddleware).forRoutes({
+      path: 'tasks/*',
+      method: RequestMethod.GET,
+    });
+  }
+  
+  constructor(private configSer : ConfigService) {}
+}
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserEntity } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+    imports : [TypeOrmModule.forFeature([UserEntity]),
+JwtModule.register(
+    {
+        secret : "supersecretcode",
+        signOptions : {
+            expiresIn : '1w'
+        }
+    }
+),
+PassportModule.register({defaultStrategy : 'Jwt'})],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController]
+})
+export class AuthModule {}
